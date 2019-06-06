b0cadc432593f36e252fc94be8cc0ee3edb5dbfe
kshvab <kshvab@gmail.com>
2019-06-06 08:18:35 +0300

    v0.2.0

--- src/index.js
+++ src/index.js
@@ -3,11 +3,153 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import * as serviceWorker from './serviceWorker';
 import DevTools from 'mobx-react-devtools';
-import { observable, computed, configure, action } from 'mobx';
+import { observable, computed, configure, action, decorate } from 'mobx';
 import { observer } from 'mobx-react';
-
 configure({ enforceActions: 'observed' });
 
+class Store {
+  devsList = [
+    { name: 'Jack', sp: 12 },
+    { name: 'Pica', sp: 10 },
+    { name: 'Bulba', sp: 8 }
+  ];
+
+  filter = '';
+
+  get totalSum() {
+    return this.devsList.reduce((sum, { sp }) => (sum += sp), 0);
+  }
+
+  get topPerformer() {
+    const maxSp = Math.max(...this.devsList.map(({ sp }) => sp));
+    return this.devsList.find(({ sp, name }) => {
+      if (maxSp === sp) {
+        return name;
+      }
+    });
+  }
+
+  get filteredDevelopers() {
+    const matchesFilter = new RegExp(this.filter, 'i');
+    return this.devsList.filter(
+      ({ name }) => !this.filter || matchesFilter.test(name)
+    );
+  }
+
+  updateFilter(value) {
+    this.filter = value;
+  }
+
+  clearList() {
+    this.devsList = [];
+  }
+
+  addDeveloper(dev) {
+    this.devsList.push(dev);
+  }
+}
+
+decorate(Store, {
+  devsList: observable,
+  filter: observable,
+  totalSum: computed,
+  topPerformer: computed,
+  filteredDevelopers: computed,
+  clearList: action,
+  addDeveloper: action,
+  updateFilter: action
+});
+
+const appStore = new Store();
+
+const Row = ({ data: { name, sp } }) => {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{sp}</td>
+    </tr>
+  );
+};
+
+@observer
+class Table extends Component {
+  render() {
+    const { store } = this.props;
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <td>Name:</td>
+            <td>SP:</td>
+          </tr>
+        </thead>
+        <tbody>
+          {store.filteredDevelopers.map((dev, i) => (
+            <Row key={i} data={dev} />
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>Team SP:</td>
+            <td>{store.totalSum}</td>
+          </tr>
+          <tr>
+            <td>Top performer:</td>
+            <td>{store.topPerformer ? store.topPerformer.name : ''}</td>
+          </tr>
+        </tfoot>
+      </table>
+    );
+  }
+}
+
+@observer
+class Controls extends Component {
+  addDeveloper = () => {
+    const name = prompt('The name:');
+    const sp = parseInt(prompt('The story points:'), 10);
+    this.props.store.addDeveloper({ name, sp });
+  };
+
+  clearList = () => {
+    this.props.store.clearList();
+  };
+
+  filterDevelopers = ({ target: { value } }) => {
+    this.props.store.updateFilter(value);
+  };
+
+  render() {
+    return (
+      <div className="controls">
+        <button onClick={this.clearList}>Clear table</button>
+        <button onClick={this.addDeveloper}>Add record</button>
+        <input
+          value={this.props.store.filter}
+          onChange={this.filterDevelopers}
+        />
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <DevTools />
+        <h1>Sprint Board</h1>
+        <Controls store={appStore} />
+        <Table store={appStore} />
+      </div>
+    );
+  }
+}
+
+{
+  // Saved
+  /*
 const nickName = observable(
   {
     firstName: 'Yauhen',
@@ -56,7 +198,9 @@ class Counter extends Component {
     );
   }
 }
+*/
+}
 
-ReactDOM.render(<Counter store={nickName} />, document.getElementById('root'));
+ReactDOM.render(<App store={Store} />, document.getElementById('root'));
 
 serviceWorker.unregister();
